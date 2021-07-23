543727c49876fa558e79712ff55d69c0088d88dc
Notfound4542 <axelcasteloot@gmail.com>
2021-07-23 15:39:52 +0200

    vazi rien

---
+++ commands/bonjour.js
@@ -0,0 +1,10 @@
+module.exports = {
+    run: message => message.channel.send('bonjour à toi'),
+    name: 'bjr',
+    help: {
+        description: 'Cette commande permet de dire bonjour ',
+        syntax: '[nom de la commande]'
+    }
+
+
+}
---
+++ commands/sbr.js
@@ -0,0 +1,5 @@
+module.exports = {
+    run: message => message.channel.send('Wesh dino arrete ptn je te dis rien '),
+    name: 'sbr',
+   
+}
